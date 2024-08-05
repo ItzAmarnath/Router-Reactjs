037538833f2866c7dcd10424c4c103223ed05495
ItzAmarnath <128959336+ItzAmarnath@users.noreply.github.com>
2024-08-05 20:23:12 +0530

    Add files via upload

---
+++ React-Router-Task-main/src/Components/InternalNav/NavInternal.jsx
@@ -0,0 +1,29 @@
+import "./NavInternal.css";
+import { Link } from "react-router-dom";
+
+const categories = [
+  { name: "All", path: "/" },
+  { name: "FullStack Development", path: "/FSD" },
+  { name: "Data Science", path: "/DS" },
+  { name: "Cyber Security", path: "/CS" },
+  { name: "Cloud Computing", path: "/CC" },
+];
+
+function NavInternal() {
+  return (
+    <>
+      <article className="nav-container">
+        <section className="nav-internal">
+          {categories.map((category) => (
+            <Link className="nav-links" key={category.name} to={category.path}>
+              {category.name}
+            </Link>
+          ))}
+        </section>
+        {/* <div className="seperator"></div>{" "} */}
+      </article>
+    </>
+  );
+}
+
+export default NavInternal;
